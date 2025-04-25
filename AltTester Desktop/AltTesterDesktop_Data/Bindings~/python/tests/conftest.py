1a931d03ee3d9279319c298228fb0cdb1c894ef0
Iuliana Todoran <iuliana.todoran@altom.com>
2025-04-25 17:47:45 +0300

    Remove wrong Desktop app

--- AltTester Desktop/AltTesterDesktop_Data/Bindings~/python/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--file_to_log", action="store", default=None,
-        help="The path to the output file of the browserstack sdk command"
-    )
